96537495dc239476f3ce931601e780d5eea4a18c
Nathaniel Hallford <hallfordnathaniel@gmail.com>
2023-09-25 18:43:26 -0500

    Updates and improvements

--- release/mysql/db-init/create-csweb-db.sql
+++ release/mysql/db-init/create-csweb-db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `census_final` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `census_final`;
+CREATE DATABASE  IF NOT EXISTS `cspro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cspro`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: census_final
